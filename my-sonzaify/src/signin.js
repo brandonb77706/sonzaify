cbf97ffaba53cb82848100bd2aa178c4bbe83da3
Brandon Brown <brandonb77706@gmail.com>
2025-05-04 01:58:24 -0400

    trying to fix redirect error

--- my-sonzaify/src/signin.js
+++ my-sonzaify/src/signin.js
@@ -4,7 +4,7 @@ import "./siginin.css";
 
 const CLIENT_ID = "2c44fa46772d42b3bc909846f3e146a2"; // Replace with your Spotify Client ID
 const REDIRECT_URI =
-  "https://sonzaify-qpvxrkn76-brandonb77706s-projects.vercel.app/callback";
+  "https://sonzaify-iqkypvp72-brandonb77706s-projects.vercel.app/callback";
 const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const SPOTIFY_API_ENDPOINT = "https://api.spotify.com/v1";
 
