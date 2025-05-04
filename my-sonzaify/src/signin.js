bf91fc92652191998ac35ab3a809f21a691e96b8
Brandon Brown <brandonb77706@gmail.com>
2025-05-04 02:04:51 -0400

    Fixing new redirect bug

--- my-sonzaify/src/signin.js
+++ my-sonzaify/src/signin.js
@@ -2,9 +2,9 @@ import React, { useEffect, useCallback } from "react";
 import { setAccessToken, setUserId } from "./globalManger.js";
 import "./siginin.css";
 
-const CLIENT_ID = "2c44fa46772d42b3bc909846f3e146a2"; // Replace with your Spotify Client ID
+const CLIENT_ID = "2c44fa46772d42b3bc909846f3e146a2";
 const REDIRECT_URI =
-  "https://sonzaify-iqkypvp72-brandonb77706s-projects.vercel.app/callback";
+  "https://sonzaify-1tcffrmip-brandonb77706s-projects.vercel.app/callback";
 const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const SPOTIFY_API_ENDPOINT = "https://api.spotify.com/v1";
 
@@ -40,7 +40,7 @@ function SignIn({ onConnect }) {
         console.error("Error fetching user profile:", error);
       }
     },
-    [onConnect] // Dependencies for useCallback
+    [] // Dependencies for useCallback
   );
 
   // Handle Spotify redirect on component mount
